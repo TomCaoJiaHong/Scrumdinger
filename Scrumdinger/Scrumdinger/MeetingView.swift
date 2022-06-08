a50a27de7d0064fb34300fb160c3431a49a3191d
TomCaoJiaHong <1841167049@qq.com>
2022-06-08 14:48:13 -0300

    Supplement Accessibility Data

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -22,13 +22,18 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
             Circle().strokeBorder(lineWidth: 24, antialiased: true)
             HStack{
                 Text("Speaker 1 of 3")
-                Spacer().frame(width: 40, height: 30)
+                Spacer().frame(width: 60, height: 30)
                 Button(action: {}){
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
