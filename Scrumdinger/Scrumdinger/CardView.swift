ff4ab23507d0878d991d73b9b3260530dc47873c
TomCaoJiaHong <1841167049@qq.com>
2022-06-09 10:56:32 -0300

    finished the Card View

--- Scrumdinger/Scrumdinger/CardView.swift
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -15,14 +15,20 @@ struct CardView: View {
             Spacer()
             HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock").padding(.trailing, 20)
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
-        CardView(scrum: scrum).background(scrum.theme.mainColor).previewLayout(.fixed(width: 400, height: 60))
+        CardView(scrum: scrum).background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 500, height: 60))
     }
 }
