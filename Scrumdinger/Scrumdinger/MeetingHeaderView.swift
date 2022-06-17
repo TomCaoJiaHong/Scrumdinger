b5a349cab19bb1351b853460721299d8a16b9a40
TomCaoJiaHong <1841167049@qq.com>
2022-06-17 18:13:53 -0300

    remove the missing function line

--- Scrumdinger/Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/Scrumdinger/MeetingHeaderView.swift
@@ -12,33 +12,32 @@ struct MeetingHeaderView: View {
     let secondsRemaining: Int
     let theme: Theme
     
-    private var totalSeconds: Int{
+    private var totalSeconds: Int {
         secondsElapsed + secondsRemaining
     }
-    
-    private var progress: Double{
-        guard totalSeconds > 0 else {return 1}
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
         return Double(secondsElapsed) / Double(totalSeconds)
     }
-    
-    private var minutesRemaining: Int{
+    private var minutesRemaining: Int {
         secondsRemaining / 60
     }
     
-    let scrumColor: Color
-    
     var body: some View {
-        VStack{
-            ProgressView(value: progress).progressViewStyle(ScrumProgressViewStyle(scrumColor: scrumColor))
-            HStack{
+        VStack {
+            ProgressView(value: progress)
+            HStack {
                 VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
+                    Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
-                    Text("Seconds Remaining").font(.caption)
-                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill").labelStyle(.trailingIcon)
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
@@ -51,7 +50,7 @@ struct MeetingHeaderView: View {
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum, scrumColor: DailyScrum.Data[0].color)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
             .previewLayout(.sizeThatFits)
     }
 }
