8e2c8f1e1f321ef9000e2bd2d172d4e6e4c49495
TomCaoJiaHong <1841167049@qq.com>
2022-06-23 22:01:43 -0300

    test

--- Scrumdinger/Scrumdinger/Models/AVPlayer+Ding.swift
+++ Scrumdinger/Scrumdinger/Models/AVPlayer+Ding.swift
@@ -12,5 +12,6 @@ extension AVPlayer {
     static let sharedDingPlayer: AVPlayer = {
         guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
         return AVPlayer(url: url)
+        
     }()
 }
