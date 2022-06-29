e9df466cdc79792f074cc75d61eed3ce24261bc1
TomCaoJiaHong <1841167049@qq.com>
2022-06-29 19:50:28 -0300

    create speakerarc file

---
+++ Scrumdinger/Scrumdinger/SpeakerArc.swift
@@ -0,0 +1,16 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by 曹嘉鸿 on 2022-06-29.
+//
+
+import SwiftUI
+
+struct SpeakerArc: Shape{
+    func path(in rect: CGRect) -> Path {
+        return Path{
+            path in
+        }
+    }
+}
